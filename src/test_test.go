f59ef4306934de32bbc7a98ca0e7729ac8a0456e
I548435 <alex.han@sap.com>
2025-06-17 05:22:23 +0000

    chore: add more comments

--- src/test_test.go
+++ src/test_test.go
@@ -9,6 +9,15 @@ import (
 
 /*
  * Zero Downtime Deployment -> Rolling deployment
+ *
+ * Assets                                    Assets
+ * +----+------+--------+                    +----+------+-----------+
+ * | id | name | source |     migration      | id | name | source id |
+ * +----+------+--------+     -------->      +----+------+-----------+
+ *                                           Sources
+ * 										     +-----------+------+
+ *                                           | source id | name |
+ *                                           +-----------+------+
  */
 func TestZeroDowntimeDeployment(t *testing.T) {
 	rq := require.New(t)
@@ -23,34 +32,91 @@ func TestZeroDowntimeDeployment(t *testing.T) {
 	mngr.Init(t.Context())
 
 	// original version
+	//
+	// Assets
+	// +----+------+--------+
+	// | id | name | source |
+	// +----+------+--------+
+	//                 RW
+	//
 	v1 := NewApp("8081", "v1", docker)
 	mngr.DeployFirstVersion(v1)
 
 	// Stage 1: Change database schema
-	// v2 -> startup:  add new schema
-	//       read:     old schema
-	//       write:    both schemas
+	//
+	// Assets
+	//						add new column
+	// +----+------+--------+-----------+
+	// | id | name | source | source id |
+	// +----+------+--------+-----------+
+	//              RW(v1/v2)    W(v2)
+	//
+	// Sources
+	// +-----------+------+
+	// | source id | name |
+	// +-----------+------+
+	//     W(v2)     W(v2)
+	//
 	v2 := NewApp("8082", "v2", docker)
 	mngr.RunZeroDowntimeDeploymentAndTest(v2)
 
 	// Stage 2: Database migration
-	// v3 -> startup:  data migration
-	//       read:     new schema
-	//       write:    both schemas
+	//
+	// Assets
+	//					      migration
+	//                  ----------------------
+	//                  |                    |
+	//                  |                    |
+	// +----+------+--------+-----------+    |
+	// | id | name | source | source id |  <-|
+	// +----+------+--------+-----------+    |
+	//               R(v2)      R(v3)        |
+	//              W(v2/v3)   W(v2/v3)      |
+	//                                       |
+	// Sources                               |
+	// +-----------+------+                  |
+	// | source id | name |       <-----------
+	// +-----------+------+
+	//    R(v3)      R(v3)
+	//   W(v2/v3)   W(v2/v3)
+	//
 	v3 := NewApp("8083", "v3", docker)
 	mngr.RunZeroDowntimeDeploymentAndTest(v3)
 
 	// Stage 3: Remove dependency on the old structure
-	// v4 -> startup:
-	//       read:     new schema
-	//       write:    new schemas
+	//
+	// Assets
+	// +----+------+--------+-----------+
+	// | id | name | source | source id |
+	// +----+------+--------+-----------+
+	//               W(v3)    RW(v3/v4)
+	// Sources
+	// +-----------+------+
+	// | source id | name |
+	// +-----------+------+
+	//   RW(v3/v4)  RW(v3/v4)
+	//
 	v4 := NewApp("8084", "v4", docker)
 	mngr.RunZeroDowntimeDeploymentAndTest(v4)
 
 	// Stage 4: clean up
-	// v5 -> startup:  remove old field
-	//       read:     new schema
-	//       write:    new schemas
+	//
+	// Assets
+	//             remove column
+	//             +--------+
+	//             | source |
+	//             +--------+
+	//             |        |
+	// +----+------+        +-----------+
+	// | id | name |        | source id |
+	// +----+------+        +-----------+
+	//                        RW(v4/v5)
+	// Sources
+	// +-----------+------+
+	// | source id | name |
+	// +-----------+------+
+	//   RW(v4/v5)  RW(v4/v5)
+	//
 	v5 := NewApp("8085", "v5", docker)
 	mngr.RunZeroDowntimeDeploymentAndTest(v5)
 }
